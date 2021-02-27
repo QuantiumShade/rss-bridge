bac438e0e8505da54a7598c7dcd051199021dde4
QuantiumShade <35575120+QuantiumShade@users.noreply.github.com>
2021-02-28 00:46:06 +0100

    Update FaunamarinBridge.php

--- bridges/FaunamarinBridge.php
+++ bridges/FaunamarinBridge.php
@@ -3,43 +3,32 @@ class FaunamarinBridge extends BridgeAbstract {
 
 	const MAINTAINER = 'bg';
 	const NAME = 'FaunamarinBridge';
-	const URI = 'https://www.faunamarincorals.de/en/animals/wysiwyg';
+	const URI = 'toms-korallen.de/shop/';
 	const CACHE_TIMEOUT = 300; // 5min
 	const DESCRIPTION = 'FaunamarinBridge';
 
 	public function collectData(){
 		$html = getSimpleHTMLDOM(self::URI)
 			or returnServerError('Could not request FaunamarinBridge.');
-
-		foreach($html->find('.col-12.col-md-4.col-lg-4') as $element) {
+//$html->find('.col-12.col-md-4.col-lg-4')
+		foreach($html->find('.post-item') as $element) {
 			$item = array();
-			$temp = $element->find('a.thumb-title.stretched-link.text-left', 0);
+			$temp = $element->find('h2.woocomerce-loop-product__title', 0);
 			$titre = html_entity_decode($temp->innertext);
 			$url = $temp->href;
 
 
-			// $temp = $element->find('img');
-			// $top = array();
-
-			// foreach($element->find('a.product_img_link > img') as $test) {
-			// 	$top=$test;
-			// }
-           
-			// $temp = $element->find('a.product_img_link img', 0);
 	
-			// $images = $temp->find('img');
-
-			// foreach($images as $image) {
-			// 	$img_src = str_replace('.jpg', '.jpg', $image->src); 
-			// 	$image->src = $img_src;
-			// }
 			
-			$tmp = $element->find('a.price', 0);
+			$image = $element->find('img', 0)->src;
+			
+			$tmp = $element->find('span.price', 0);
 			$price = html_entity_decode($tmp->innertext);
 
 			
 
-			$item['content'] = 'default';
+		
+			$item['content'] = '<img src="' . $image . '">';
 	
 			$item['uri'] = $url;
 			$item['title'] = trim($titre) . ' | ' . trim($price);
