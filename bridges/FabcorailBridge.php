956186ece234e0d77bea05be8848ab64ee565b82
QuantiumShade <35575120+QuantiumShade@users.noreply.github.com>
2021-02-27 21:01:33 +0100

    Update FabcorailBridge.php

--- bridges/FabcorailBridge.php
+++ bridges/FabcorailBridge.php
@@ -21,7 +21,7 @@ class FabcorailBridge extends BridgeAbstract {
 			$temp = $element->find('a.product_img_link', 0);
 
 			foreach($temp->find('img')) as $img) {
-				$images = $images . $img
+				$images = $images . $img;
 			}
 
 			foreach($images as $image) {
