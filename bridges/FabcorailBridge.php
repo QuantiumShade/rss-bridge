59a8957820fb31e2587f782ed36889996ce63d99
QuantiumShade <35575120+QuantiumShade@users.noreply.github.com>
2021-02-27 21:48:29 +0100

    Update FabcorailBridge.php

--- bridges/FabcorailBridge.php
+++ bridges/FabcorailBridge.php
@@ -26,7 +26,7 @@ class FabcorailBridge extends BridgeAbstract {
 			// 	$img_src = str_replace('.jpg', '.jpg', $image->src); 
 			// 	$image->src = $img_src;
 			// }
-			$temp2= $element->find('img.replace-2x', 1);
+			$temp2= $element->find('img', 1);
 
 			$content = $temp->innertext;
 			$content2 = $temp2->innertext;
