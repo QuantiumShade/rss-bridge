e6befe60a021b98f18f630bf6efb695bff8ed572
QuantiumShade <35575120+QuantiumShade@users.noreply.github.com>
2021-02-27 23:56:57 +0100

    Create FaunamarinBridge.php

---
+++ bridges/FaunamarinBridge.php
@@ -0,0 +1,51 @@
+<?php
+class FaunamarinBridge extends BridgeAbstract {
+
+	const MAINTAINER = 'bg';
+	const NAME = 'FaunamarinBridge';
+	const URI = 'https://www.faunamarincorals.de/en/animals/wysiwyg';
+	const CACHE_TIMEOUT = 300; // 5min
+	const DESCRIPTION = 'FaunamarinBridge';
+
+	public function collectData(){
+		$html = getSimpleHTMLDOM(self::URI)
+			or returnServerError('Could not request FaunamarinBridge.');
+
+		foreach($html->find('article.cmp') as $element) {
+			$item = array();
+			$temp = $element->find('a.thumb-title', 0);
+			$titre = html_entity_decode($temp->innertext);
+			$url = $temp->href;
+
+
+			// $temp = $element->find('img');
+			// $top = array();
+
+			// foreach($element->find('a.product_img_link > img') as $test) {
+			// 	$top=$test;
+			// }
+           
+			// $temp = $element->find('a.product_img_link img', 0);
+	
+			// $images = $temp->find('img');
+
+			// foreach($images as $image) {
+			// 	$img_src = str_replace('.jpg', '.jpg', $image->src); 
+			// 	$image->src = $img_src;
+			// }
+			
+			$tmp = $element->find('a.price', 0);
+			$price = html_entity_decode($tmp->innertext);
+
+			
+
+			$item['content'] = 'default';
+	
+			$item['uri'] = $url;
+			$item['title'] = trim($titre) . ' | ' . trim($price);
+
+
+			$this->items[] = $item;
+		}
+	}
+}
